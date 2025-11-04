8619b5028a13f02a9fc35c7e3855013a139926cc
tyang019 <tyang019@fiu.edu>
2025-11-03 21:44:08 -0500

    Initialized new changes

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-const searchButton = document.querySelector('.search-button');
-
+//1. Select DOM element to work with
 const searchBar = document.getElementById('input');
-
+const searchButton = document.querySelector('.search-button');
 const searchResult = document.getElementById('search-result');
+const overlay = document.getElementById('overlay');
+const confirmBtn = document.getElementById('confirmBtn');
+
+
+//2. Add Event Listener 
+confirmBtn.addEventListener('click', function(){
+ overlay.style.display = 'none';
+})
 
 searchButton.addEventListener('click', () => {
   const query = searchBar.value.trim();
   if(query === ""){
-    alert("Please type something into the search bar");
+   overlay.style.display = 'flex';
   }else{
      searchResult.textContent = `You searched for: "${query}"`;
   }
 });
 
-orderButton.addEventListener('click', () => {
-  
-})
+
+//3. Functions
+
+
+//4. Input Validation
+
+
+//5 Business Logic
+
+
+//6. DOM Manipulation 
+
+
+//7. Optional Utility Functions
